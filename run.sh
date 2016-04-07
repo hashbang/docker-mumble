d70d4af65d148aa00f173c696358ad5a6b2f81ab
The Fox in the Shell <KellerFuchs@hashbang.sh>
2016-04-07 16:47:27 +0200

    run.sh: Variable quoting
    Since the password is generated using `pwgen -n`, this shouldn't
      be an issue, but this is less brittle.

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ touch /var/lib/mumble-server
 
 
 if [ -z "$SUPW" ]; then
-    SUPW=`pwgen -c -n -1 15`
+    SUPW="$(pwgen -c -n -1 15)"
 fi
 
 echo "Superuser Password: $SUPW"
@@ -13,10 +13,10 @@ echo "Superuser Password: $SUPW"
 #exec sumurmurd -ini /etc/mumble-server.ini -supw $SUPW
 #exec sudo -u mumble murmurd -ini /etc/mumble-server.ini -fg
 
-/usr/sbin/murmurd -supw $SUPW
+/usr/sbin/murmurd -supw "$SUPW"
 /usr/sbin/murmurd -fg
 
 #/usr/bin/murmurd -ini /etc/mumble-server.ini -fg
-#murmurd -ini /etc/mumble-server.ini -supw $SUPW -fg
-#/usr/sbin/murmurd -fg -supw $SUPW
+#murmurd -ini /etc/mumble-server.ini -supw "$SUPW" -fg
+#/usr/sbin/murmurd -fg -supw "$SUPW"
 
